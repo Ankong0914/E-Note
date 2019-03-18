d7f95f332c08b846a6bc1e468dca73601aef0428
kobayashi <kobayashi@otocin.local>
2019-03-18 22:33:04 +0900

    Initial Commit

---
+++ E-Note/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  E-Note
+//
+//  Created by kobayashi on 2019/03/18.
+//  Copyright © 2019 kobayashi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
