8dfd8ab9fc4d61ebe810cf32019d5fcc26b9b9a6
kobayashi <kobayashi@otocin.local>
2019-03-18 23:44:23 +0900

    add a function for creating a text box at click position

--- E-Note/ViewController.swift
+++ E-Note/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextViewDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,25 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func mouseDown(with event: NSEvent) {
+        let clickPos = CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y)
+        createTextBox(x:clickPos.x, y:clickPos.y)
+    }
+    
+    func createTextBox(x: CGFloat, y: CGFloat){
+        let textView=NSTextView.init(frame: NSRect.init(x: 100, y:100, width: 15, height: 15))
+        
+        textView.backgroundColor = NSColor.systemPink
+        textView.textColor = NSColor.black
+        textView.insertionPointColor = NSColor.black
+        textView.isHorizontallyResizable = true
+        textView.frame = CGRect(x:x-4,y:y-9,width:100,height:15)
 
+        textView.delegate = self
+        
+        self.view.addSubview(textView)
+    }
 
 }
 
